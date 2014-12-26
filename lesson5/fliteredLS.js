7c185e4348d03c2d2d4d049255b379e2ca5e206a
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-26 22:20:04 +0000

    half way through lesson 5. issue with final check returning only file type

--- lesson5/fliteredLS.js
+++ lesson5/fliteredLS.js
@@ -1,1 +1,12 @@
 var fs = require('fs');
+
+var directory = process.argv[2];
+var extension = process.argv[3];
+
+fs.readdir(directory, function(error, list){
+  list.forEach(function(file) {
+    if(file.split('.').pop() === extension) {
+      console.log(file);
+    }
+  });
+});
