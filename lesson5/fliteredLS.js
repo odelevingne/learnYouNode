2c354a902e70c680cd037bd12a3e54c83b330452
Oliver Delevingne <oliver@delevingne.co.uk>
2014-12-26 22:27:35 +0000

    lesson 5 passed

--- lesson5/fliteredLS.js
+++ lesson5/fliteredLS.js
@@ -1,11 +1,12 @@
 var fs = require('fs');
+var path = require('path');
 
 var directory = process.argv[2];
 var extension = process.argv[3];
 
 fs.readdir(directory, function(error, list){
   list.forEach(function(file) {
-    if(file.split('.').pop() === extension) {
+    if('.' + extension === path.extname(file)) {
       console.log(file);
     }
   });
